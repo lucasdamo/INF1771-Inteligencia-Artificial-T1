f78ef66523beb04999a652c17205044f2d00fcc6
Esduard <eduardomotta528@gmail.com>
2021-04-19 23:14:29 -0300

    removing old stuff
    adding videos for more fluence

--- Tilemap/Sprite-arrows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Sprite-arrows" tilewidth="16" tileheight="16" tilecount="5723" columns="59">
- <image source="../imagens/Sprite-arrows.png" width="950" height="1560"/>
-</tileset>
